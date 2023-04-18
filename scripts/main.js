a7e88a5d689934d22117d98bb3c254ec40a4497c
OutCoy <cizikasg@gmail.com>
2023-04-18 10:57:01 +0300

    Pridetas intro tab isjungimo funkcionalumas

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,23 @@ const aboutTabBtn = document.querySelector('#aboutTabBtn');
 const contactTabBtn = document.querySelector('#contactTabBtn');
 
 const introTab = document.querySelector('#introTab');
+// const workTab = document.querySelector('#workTab');
+// const aboutTab = document.querySelector('#aboutTab');
+// const contactTab = document.querySelector('#contactTab');
 
 const landingPage = document.querySelector('#landingPage');
+
+const closeTab = document.querySelector('.closeTab');
 
 introTabBtn.addEventListener('click', () => {
     landingPage.classList.add('displayNone');
     introTab.classList.remove('displayNone');
+});
+
+closeTab.addEventListener('click', () => {
+    landingPage.classList.remove('displayNone');
+    introTab.classList.add('displayNone');
+    // workTab.classList.add('displayNone');
+    // aboutTab.classList.add('displayNone');
+    // contactTab.classList.add('displayNone');
 });
